3ca326ca5460fc5c5defb852048a1109a430e97b
ArisatoX <patricksungkharisma27@gmail.com>
2019-12-14 13:17:16 +0700

    new limfosit b limfosit t

--- index.js
+++ index.js
@@ -16,8 +16,8 @@
 
     //Controls
     var controls = new THREE.OrbitControls( camera, renderer.domElement );
-    camera.position.set(92.83968629417194, 9.610419921941201, -36.24130899572845);
-    // camera.position.set(0, 0, 15);
+    // camera.position.set(92.83968629417194, 9.610419921941201, -36.24130899572845);
+    camera.position.set(0, 0, 15);
     camera.lookAt(0,0,0);
     controls.update();
 
@@ -90,12 +90,12 @@
         //     my_loader('assets/MONOSIT.mtl', 'assets/MONOSIT.obj', index);
         // }
         else if(index == 4){
-            my_loader('assets/PLATELET.mtl', 'assets/PLATELET.obj', index);
+            my_loader('assets/LIMFOSITT.mtl', 'assets/LIMFOSITT.obj', index);
         }
-        else if(index == 5){
-            my_loader('assets/MAKROFAG.mtl', 'assets/MAKROFAG.obj', index);
-        }
-        else if(index==6)
+        // else if(index == 5){
+        //     my_loader('assets/MAKROFAG.mtl', 'assets/MAKROFAG.obj', index);
+        // }
+        else if(index==5)
         {
             var timer = new Date();
             end = timer.getTime();
@@ -134,7 +134,12 @@
                     // called when resource is loaded
                     function ( object ) 
                     {
-                        object.position.z += 10*index;
+
+                        if(index == 4)
+                        {
+                            object.position.set(0,0,0);
+                        }
+                        // object.position.z += 10*index;
                         // object.position.y -= 2;
                         meshes.push(object);
                         // scene.add(object);
